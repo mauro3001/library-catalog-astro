7f554fddcd39c9070e53e57ed76f476215ad80e5
mauro3001 <mauricio2182049@correo.uis.edu.co>
2024-05-16 10:27:47 -0500

    data about books

---
+++ src/data/books.js
@@ -0,0 +1,38 @@
+
+export const books = [
+    {
+      id: "pragmatic-programmer",
+      title: "The Pragmatic Programmer",
+      img: "https://images-na.ssl-images-amazon.com/images/I/41as+WafrFL._SX258_BO1,204,203,200_.jpg",
+      summary: "The Pragmatic Programmer: Your Journey to Mastery, 20th Anniversary Edition (2nd Edition) 2nd Edition",
+      author: "Andrew Hunt, David Thomas",
+    },
+    {
+      id: "clean-code",
+      title: "Clean Code",
+      img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1436202607i/3735293.jpg",
+      summary: "Even bad code can function. But if code isn't clean, it can bring a development organization to its knees. Every year, countless hours and significant resources are lost because of poorly written code. But it doesn't have to be that way.",
+      author: "Robert C. Martin",
+    },
+    {
+      id: "refactoring",
+      title: "Refactoring",
+      img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1539022493i/42245734.jpg",
+      summary: "Refactoring: Improving the Design of Existing Code (2nd Edition) 2nd Edition",
+      author: "Martin Fowler",
+    },
+    {
+      id: "design-patterns",
+      title: "Design Patterns",
+      img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1348027904i/85009.jpg",
+      summary: "Design Patterns: Elements of Reusable Object-Oriented Software 1st Edition",
+      author: "Erich Gamma, Richard Helm, Ralph Johnson, John Vlissides",
+    },
+    {
+      id: "programming-pearls",
+      title: "Programming Pearls",
+      img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1385191091i/52084.jpg",
+      summary: "The first edition of Programming Pearls was one of the most influential books I read early in my career, and many of the insights I first encountered in that book stayed with me long after I read it. Jon has done a wonderful job of updating the material.",
+      author: "Jon Bentley",
+    }
+  ];
